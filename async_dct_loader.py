da5fc7e252bf727502e86e09d1d69f507220d13d
Oliver Zendel <oliver.zendel@ait.ac.at>
2021-10-01 10:42:48 +0200

    preparing visualizations/figures for paper

--- async_dct_loader.py
+++ async_dct_loader.py
@@ -3,6 +3,9 @@ from tqdm.notebook import tqdm as tqdm_nb
 import cv2
 import numpy as np
 import asyncio
+from PIL import Image as pilim
+#original phash by calling pip install imagehash
+from imagehash import phash as pip_phash_call
 
 #global objects allow parallel executions
 nhcpp_objs_g = None
@@ -26,14 +29,24 @@ def load_img_tar(fn):
             return None
     return im
 
-async def get_dct_parallel(fn, obj_idx, dct_dim, min_img_dims, ret_hash):
+def pip_phash(i, hash_size=8):
+    return np.packbits(np.uint8(pip_phash_call(pilim.fromarray(i), hash_size).hash.flatten()))
+
+def nap_resize(i,trg_wh):
+    return np.array(pilim.fromarray(i).resize(trg_wh, pilim.LANCZOS))
+
+async def get_dct_parallel(fn, obj_idx, dct_dim, min_img_dims, ret_hash, pil_sz=-1, phsz=0):
     global nhcpp_objs_g
     try:
         i = load_img_tar(fn)
         if i is None or min(i.shape[:2]) < min_img_dims: #skip invalid or tiny images
             return None
+        if pil_sz > 0:
+             i = nap_resize(i,(pil_sz,pil_sz))
     except Exception as e:
         return None
+    if ret_hash and phsz > 0:
+        return pip_phash(i,phsz)
     dct_crop0 = np.zeros((dct_dim,dct_dim), np.float32)
     nhcpp_objs_g[obj_idx].get_dct(i,dct_crop0)
     if ret_hash:
@@ -53,7 +66,7 @@ async def get_hash_dct_parallel(dct, obj_idx):
 
 #call result = await async_load_dct_paths(..) from jupyter nb
 #other wise use result = asyncio.run(async_load_dct_paths(...))
-async def async_load_dct_paths(nhcpp_objs, all_paths, num_threads=8, dct_dim=32, min_img_dims=128, tqdm_vers = tqdm_nb, ret_hash=False):
+async def async_load_dct_paths(nhcpp_objs, all_paths, num_threads=8, dct_dim=32, min_img_dims=128, tqdm_vers = tqdm_nb, ret_hash=False, pil_sz=-1, phsz=0):
     global nhcpp_objs_g
     assert len(nhcpp_objs) == num_threads, "Number of objects in nhcpp_objs must be the same as num_threads"
     num_threads = min(len(all_paths),num_threads)
@@ -63,8 +76,8 @@ async def async_load_dct_paths(nhcpp_objs, all_paths, num_threads=8, dct_dim=32,
     for i in tqdm_vers(range(num_passes)):
         i0, tasks = i*num_threads, []
         for t in range(num_threads):
-            p = min(i0+t,len(all_paths))
-            tasks.append(asyncio.create_task(get_dct_parallel(all_paths[p],t, dct_dim, min_img_dims, ret_hash)))
+            p = min(i0+t,len(all_paths)-1)
+            tasks.append(asyncio.create_task(get_dct_parallel(all_paths[p],t, dct_dim, min_img_dims, ret_hash, pil_sz, phsz)))
         for t in range(num_threads):
             results.append(await tasks[t])
     #curiously, joblib did not work and produced a runtime comparable to a single core
@@ -83,7 +96,7 @@ async def async_hash_dcts(nhcpp_objs, all_dcts, num_threads=8, tqdm_vers = tqdm_
     for i in tqdm_vers(range(num_passes)):
         i0, tasks = i*num_threads, []
         for t in range(num_threads):
-            p = min(i0+t,len(all_dcts))
+            p = min(i0+t,len(all_dcts)-1)
             tasks.append(asyncio.create_task(get_hash_dct_parallel(all_dcts[p],t)))
         for t in range(num_threads):
             results.append(await tasks[t])
---
+++ matplot_helper.py
@@ -0,0 +1,97 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import json
+from tqdm.notebook import tqdm as tqdm_nb
+
+from async_dct_loader import load_img_tar
+import cv2
+
+bit_counts = np.array([int(bin(x).count("1")) for x in range(256)]).astype(np.uint8)
+def hamming_dist(a,b,axis=None):
+    return np.sum(bit_counts[np.bitwise_xor(a,b)],axis=axis)
+def hex2hash(s):
+    return np.frombuffer(bytes.fromhex(s), dtype=np.uint8)
+def hash2hex(h):
+    return h.tobytes().hex()
+def calc_inter_dist(hashes, eye_val=1024):
+    all_d = np.zeros((hashes.shape[0],hashes.shape[0]),dtype=np.int32)
+    for i in range(hashes.shape[0]):
+        all_d[i,i+1:] = hamming_dist(hashes[i],hashes[(i+1):], axis=1)
+    return all_d+all_d.T+np.eye(hashes.shape[0],dtype=np.int32)*eye_val
+
+def load_cntr(path0, dim=128):
+    i0 = load_img_tar(path0)
+    fx = dim/i0.shape[0]
+    x0, y0 = (int(i0.shape[1]*fx+0.5)-dim)//2, 0
+    if i0.shape[0] > i0.shape[1]:
+        fx = dim/i0.shape[1]
+        x0, y0 = 0, (int(i0.shape[0]*fx+0.5)-dim)//2
+    return cv2.resize(i0,(0,0),fx=fx,fy=fx,interpolation=cv2.INTER_AREA)[y0:(y0+dim),x0:(x0+dim)]
+
+def vis_pairs(pairs0,paths, dim=128):
+    loaded_pairs = []
+    spacer=np.ones((dim,dim//8,3),np.uint8)*255
+    for (p0,p1) in pairs0:
+        i0 = load_cntr(paths[p0], dim=dim)
+        i1 = load_cntr(paths[p1], dim=dim)
+        loaded_pairs+=[i0,i1,spacer]
+    return loaded_pairs
+
+def cls_fig():
+    plt.close() 
+    plt.subplots(figsize=(16, 9))
+    plt.gca().xaxis.set_major_locator(mticker.MultipleLocator(1))
+
+def get_hash_dists(h, name0, paths, distname='nap0'):
+    name0 = name0+'_'
+    dists0 = calc_inter_dist(h,1024)
+    dists0 = np.float32(dists0)
+    dists0[dists0 > 1023] = np.nan
+    m_mean = np.int32(np.nanmean(dists0,axis= 1)+0.5)
+    m_max = np.int32(np.nanmax(dists0,axis= 1)+0.5)
+    m_min = np.int32(np.nanmin(dists0,axis= 1)+0.5)
+    v0,c0 = np.unique(m_mean, return_counts=True)
+    v1,c1 = np.unique(m_max, return_counts=True)
+    v2,c2 = np.unique(m_min, return_counts=True)
+
+    plt.subplots(figsize=(16, 9))
+    for store_one in [True,False]:
+        for (vals, counts, name1) in [(v0,c0,'mean'),(v1,c1,'max'),(v2,c2,'min')]:
+            if store_one: cls_fig()
+            if len(vals) < 6:
+                if min(vals) > 2:
+                    vals = [(min(vals)-2),(min(vals)-1)]+vals.tolist()+[(max(vals)+1),(max(vals)+2)]
+                    counts = [0,0]+c0.tolist()+[0,0]
+                else:
+                    vals = vals.tolist()+[(max(vals)+i) for i in range(1,5)]
+                    counts = c0.tolist()+[0,0,0,0]
+            plt.bar(vals, counts, label=name0+name1+' dists')
+            plt.legend()
+            if store_one: plt.savefig('hist_'+distname+'_'+name0+name1+'.png')  
+        if not store_one: plt.savefig('hist_'+distname+'_'+name0+'all.png')
+        cls_fig()
+    #show nearest hits
+    np.nan_to_num(dists0, nan=1024, copy=False)
+    dists0 = np.int32(dists0)
+    pairs0, dists0 = find_closest(m_min, dists0)
+    json.dump({'pairs':pairs0,'dists':dists0,'name0':name0}, open('hist_'+distname+'_nearest_'+name0+'.json','wt'))
+    nearest_matches = np.hstack(vis_pairs(pairs0,paths))
+    cv2.imwrite('hist_'+distname+'_nearest_'+name0+'.jpg',nearest_matches)
+    
+def find_closest(m_min, c0, max_num=8):
+    solved_pairs = []
+    ret_pairs, ret_dists = [], []
+    best_ord = np.argsort(m_min)
+    for p0 in best_ord:
+        if p0 in solved_pairs:
+            continue
+        p1 = np.argmin(c0[p0])
+        if p1 in solved_pairs:
+            continue
+        ret_pairs.append((int(p0),int(p1)))
+        solved_pairs.append(p0)
+        solved_pairs.append(p1)
+        ret_dists.append(int(c0[p0,p1]))
+        if len(ret_pairs) >= max_num:
+            break
+    return ret_pairs, ret_dists
